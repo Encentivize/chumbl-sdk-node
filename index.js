1e8acae5c97ada61f2b45aaf7aa5c60e4c1e0799
Ryan Kotzen <Ryan@encentivize.co.za>
2016-04-15 08:11:42 +0200

    moving chumbl file into index.

--- index.js
+++ index.js
@@ -1,4 +1,41 @@
-﻿module.exports = require('./lib/chumbl.js');
+﻿'use strict';
 
+var metric = require('./lib/metric');
+var libraryChomp = require('./lib/library-chomp.js');
+var timestore = require('./lib/timestore.js');
+var datastore = require('./lib/datastore.js');
+var transaction = require('./lib/transaction.js');
+var lookup = require('./lib/lookup.js');
+var chomp = require('./lib/chomp.js');
+var variables = require('./lib/variables.js');
+var users = require('./lib/users.js');
+var chumblr = require('./lib/chumblr.js');
+var inventory = require('./lib/inventory.js');
+var configServices = require('./lib/config-services.js');
+var groups = require('./lib/groups.js');
 
-
+module.exports = {
+    timestore: timestore,
+    timestores: timestore,
+    transaction: transaction,
+    transactions: transaction,
+    chomp: chomp,
+    chomps: chomp,
+    lookup: lookup,
+    lookups: lookup,
+    variables: variables,
+    users: users,
+    datastores: datastore,
+    datastore: datastore,
+    chumblr: chumblr,
+    chumblrs: chumblr,
+    libraryChomp: libraryChomp,
+    libraryChomps: libraryChomp,
+    inventory: inventory,
+    inventories: inventory,
+    configServices: configServices,
+    metric: metric,
+    metrics: metric,
+    group: groups,
+    groups: groups
+};
